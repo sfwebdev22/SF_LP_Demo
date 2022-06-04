7b1af4b44792892c9035a95e1681e87dbe01cdcc
sfwebdev22 <105599618+sfwebdev22@users.noreply.github.com>
2022-06-04 16:55:41 -0400

    Added filter for sorting/ordering bullets

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,15 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/admin');
 
+    // Use only the first 3 chars of month
     eleventyConfig.addFilter('shortMonth', function (eventMonth) {
         return eventMonth.slice(0, 3);
+    });
+
+    // Sort bullets by given order - IE order: 1
+    eleventyConfig.addFilter('sortByOrder', function (values) {
+        let vals = [...values];
+        return vals.sort((a, b) => Math.sign(a.data.order - b.data.order));
     });
 
     return {
